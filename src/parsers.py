7b1abcd832d4e10c1f94720b50a758b853662d0d
MarySnik <mary.snikk@gmail.com>
2026-03-18 09:39:41 +0800

    pref: finished parsers.py

--- src/parsers.py
+++ src/parsers.py
@@ -37,41 +37,43 @@ def parse_to_file(full_info: dict | None = None, json_info: str | None = None) -
                 fp.write(json_info)
             logging.info("saved")
         return temp_file
-    except FileNotFoundError as e:
-        logging.error(f"[PARSERS] The file not found: {e} ") #actually it's impossible. but what if
-
+    except TypeError as e:
+        logging.error(f"pparsers: type error")
+        raise TypeError
 
 def parsed_res(all_segments: list | None = None, info: dict | None = None, filename: str | None = None, fetched_transcript: list | None = None) -> dict:
 
-    if all_segments is not None:
-        dict_res_list = [{
-                'start_t': utils.formatting_seconds(s.start),
-                'end_t':   utils.formatting_seconds(s.end),
-                'content': s.text,
-            } for s in all_segments
-        ]
+    try: #
+        if all_segments is not None:
+            dict_res_list = [{
+                    'start_t': utils.formatting_seconds(s.start),
+                    'end_t':   utils.formatting_seconds(s.end),
+                    'content': s.text,
+                } for s in all_segments
+            ]
 
-        return {
-            "filename": filename,
-            "duration": utils.formatting_seconds(info.duration),
-            "language": info.language.upper(),
-            "language Probability": f"{info.language_probability:.1%}",
-            "transcript": dict_res_list
-        }
-    elif all_segments is None and fetched_transcript:
-        dict_res_list = [{
-                'start_t': utils.formatting_seconds(s.start),
-                'end_t':   utils.formatting_seconds(s.start + s.duration),
-                'content': s.text 
-            } for s in fetched_transcript
-        ]
+            return {
+                "filename": filename,
+                "duration": utils.formatting_seconds(info.duration),
+                "language": info.language.upper(),
+                "language Probability": f"{info.language_probability:.1%}",
+                "transcript": dict_res_list
+            }
+        elif all_segments is None and fetched_transcript:
+            dict_res_list = [{
+                    'start_t': utils.formatting_seconds(s.start),
+                    'end_t':   utils.formatting_seconds(s.start + s.duration),
+                    'content': s.text 
+                } for s in fetched_transcript
+            ]
 
-        return {
-            "transcript": dict_res_list
-        }
+            return {
+                "transcript": dict_res_list
+            }
+        else: raise ValueError
 
-    else:
-        raise Exception(f'[parsers] No information to parse: {Exception}')
+    except:
+        raise AttributeError(f'[parsers] No information to parse:')
 
         
         
--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,28 +1,33 @@
 import pytest
-import src.parsers as parsers
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock, patch, mock_open
 from pathlib import Path
+with patch('faster_whisper.WhisperModel'):
+    import src.parsers as parsers
 #functions
 # 1) save_file(source)
 # - check that it really saves file
 # - check what if the file is corrupted - no need - it's always saves everytohng
 # - check if there're None
-def test_save_file_default():
-    fake = Mock()
+
+def write_to_file(path: str, content: bytes):
+    with open (path, 'wb') as handle:
+        handle.write(content)
+
+def test_save_to_file_default():
     path = '/home/snikmas/video_1.mp4'
-    with open(path, 'rb') as f:
-        real_bytes = f.read()
+    fake_content = b'line1\nline1\n'
+    m = mock_open()
 
-    fake.return_value = real_bytes
-    fake.filename = 'name'
+    with patch('builtins.open', m):
+        result = write_to_file(path, fake_content)
 
-    temp_file_path = parsers.temp_dir / f'temp_{fake.filename}'
-
-    assert parsers.save_file(fake) == temp_file_path
-
+    m.assert_called_once_with(path, 'wb') # right arguments?
+    m().write.assert_called_once_with(fake_content)
+    
+        
 def test_save_file_none():
     with pytest.raises(AttributeError):
-        parsers.save_file(None)
+        parsers.save_file(None) #i tihnk i have to pass mock
         
 # 2) prase_to_file()
 # - check that it works for full_info / json_info
@@ -39,29 +44,57 @@ def test_parse_to_file_json_info():
     assert isinstance(path_res, Path)
 
 def test_parse_to_file_none():
-    with pytest.raises(FileNotFoundError):
-        parsers.parse_to_file()
+    with pytest.raises(TypeError):
+        parsers.parse_to_file(None, None)
 
 # 3) parsed_res
-# - check if all_gsegments
-# - check if tf ther're fetched transcript
+# - check if all_gsegments ok 
+# - check if tf ther're fetched transcript ok
 # - check if everytihng there (but maybe no need to check, there're if elif else statement, nohting wrong
 # - check what if there' nohting)
 # - check what if the data doesn't exist/ corrupted
-def test_parsed_res_default():
-    all_segments = [
-        {
-            'start': 14,
-            'end': 16,
-            'text': 'text_one',
-            'start': 16,
-            'end': 134,
-            'text': 'text_two',
-         }
-    ]
-    info = {
-        'language': 'en',
-        'language_probability': 'wow' #idk how to do that 
-    }
+
+def create_segment_mock(start, end, text, duration):
+    seg = MagicMock()
+    seg.start = start
+    seg.end = end
+    seg.duration = duration
+    seg.text = text
+    return seg
+
+def test_parsed_res_default(): # crate a test with broken? idk
+    # not good? cuz it checks for types, not for json
+    all_segments = []
+    all_segments.append(create_segment_mock(12, 23, "this is the best text ever", 123))
+    all_segments.append(create_segment_mock(32, 344, "this is the second best text ever", 555))
+    
+    # or do mock type?
+    info = Mock()
+    info.duration = 20
+    info.language = 'en'
+    info.language_probability = 0.32
+
     res = parsers.parsed_res(all_segments=all_segments, filename="file_name", info=info)
     assert isinstance(res, dict)
+
+def test_parsed_res_only_fetched_transcript():
+    fetched_transcript = []
+    fetched_transcript.append(create_segment_mock(12, 122, "this is the best text ever", 123))
+    fetched_transcript.append(create_segment_mock(124, 134, "this is the second best text ever", 555))
+    
+    res = parsers.parsed_res(fetched_transcript=fetched_transcript, filename="file_name")
+
+    assert isinstance(res, dict)
+
+def test_parsed_res_none_returns_type_error():
+    with pytest.raises(TypeError):
+        parsers(None, None, None, None)
+
+def test_parsed_res_if_not_full_data():
+    all_segments = []
+    all_segments.append(create_segment_mock(12, 32, "this is the best text ever", 123))
+    all_segments.append(create_segment_mock(123, 324, "this is the second best text ever", 555))
+    
+    with pytest.raises(AttributeError):
+        parsers.parsed_res(all_segments=all_segments)
+    
